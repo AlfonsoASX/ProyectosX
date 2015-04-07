69ad4c8d16a1b662f7cd7c5aa6c534fcd354e2df
Alfonso Sánchez <a@asx.mx>
2015-04-06 21:30:50 -0500

    Carga inicial INDEX

---
+++ index.php
@@ -0,0 +1,83 @@
+
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Alfonso Sánchez">
+    <link rel="icon" href="favicon.ico">
+    <title>Volanteo efectivo</title>
+    <link href="3rasPartes/bootstrap/3/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="masthead">
+        <h3 class="text-muted">Volanteo Efectivo</h3>
+        <nav>
+          <ul class="nav nav-justified">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Downloads</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-12">
+<div id="volantes" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#volantes" data-slide-to="0" class="active"></li>
+    <li data-target="#volantes" data-slide-to="1"></li>
+    <li data-target="#volantes" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="..." alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item">
+      <img src="..." alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    ...
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#volantes" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#volantes" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+        </div>
+      </div>
+
+      <!-- Site footer -->
+      <footer class="footer">
+        <p>Volanteo Efectivo 2015</p>
+      </footer>
+
+    </div> <!-- /container -->
+    <script src="3rasPartes/jquery/1.11.2/jquery.min.js"></script>
+    <script src="3rasPartes/bootstrap/3/js/bootstrap.min.js"></script>
+  </body>
+</html>
